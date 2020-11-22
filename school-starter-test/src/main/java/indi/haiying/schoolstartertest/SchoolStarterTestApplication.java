2982a3cb8ce46c283a7f180e5c96fd18638187ac
haiying <duhaiying@qq.com>
2020-11-22 20:19:12 +0800

    项目结构等调整

--- school-starter-test/src/main/java/indi/haiying/schoolstartertest/SchoolStarterTestApplication.java
+++ school-starter-test/src/main/java/indi/haiying/schoolstartertest/SchoolStarterTestApplication.java
@@ -1,15 +1,17 @@
 package indi.haiying.schoolstartertest;
 
 import indi.haiying.springbootschoolstarter.School;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SchoolStarterTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolStarterTestApplication.class, args);
-		System.out.println(BeanUtil.getBean(School.class));
+		log.warn("school: {}", BeanUtil.getBean(School.class));
 	}
 
 }
