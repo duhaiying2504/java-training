524738635b66819187033affd3ad1533c5cb3177
haiying <duhaiying@qq.com>
2020-11-22 20:20:19 +0800

    使用try with resource调整

--- jdbcs/src/main/java/indi/haiying/jdbcs/dao/JdbcDao.java
+++ jdbcs/src/main/java/indi/haiying/jdbcs/dao/JdbcDao.java
@@ -1,6 +1,7 @@
 package indi.haiying.jdbcs.dao;
 
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.sql.DataSource;
@@ -8,6 +9,7 @@ import java.sql.*;
 import java.util.*;
 
 @Setter
+@Slf4j
 public class JdbcDao implements Dao {
 
     @Autowired
@@ -22,21 +24,17 @@ public class JdbcDao implements Dao {
 
     public List<Map<String, Object>> select(String sql, Object... params) {
 
-        Connection connection = null;
-        ResultSet resultSet = null;
         List<Map<String, Object>> result = new ArrayList<>();
-        try {
-            connection = this.getConnection();
-            PreparedStatement preparedStatement = this.preparedStatement(sql, connection, params);
-            resultSet = preparedStatement.executeQuery();
+        try (
+                Connection connection = this.dataSource.getConnection();
+                PreparedStatement preparedStatement = this.preparedStatement(sql, connection, params)
+        ) {
+            ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 result.add(this.toMap(resultSet));
             }
-
         } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            this.closeConnection(connection);
+            log.error("", e);
         }
 
         return result;
@@ -44,21 +42,17 @@ public class JdbcDao implements Dao {
 
     public Map<String, Object> selectOne(String sql, Object... params) {
 
-        Connection connection = null;
-        ResultSet resultSet = null;
         Map<String, Object> result = null;
-        try {
-            connection = this.getConnection();
-            PreparedStatement preparedStatement = this.preparedStatement(sql, connection, params);
-            resultSet = preparedStatement.executeQuery();
+        try (
+                Connection connection = this.dataSource.getConnection();
+                PreparedStatement preparedStatement = this.preparedStatement(sql, connection, params)
+        ) {
+            ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
                 result = this.toMap(resultSet);
             }
-
         } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            this.closeConnection(connection);
+            log.error("", e);
         }
 
         return result;
@@ -66,26 +60,20 @@ public class JdbcDao implements Dao {
 
     public int update(String sql, Object... params) {
 
-        Connection connection = null;
-
         int updated = 0;
-        try {
-            connection = this.getConnection();
+        try (Connection connection = this.dataSource.getConnection()) {
             connection.setAutoCommit(false);
-            PreparedStatement preparedStatement = this.preparedStatement(sql, connection, params);
-            updated = preparedStatement.executeUpdate();
-            connection.commit();
-        } catch (SQLException e) {
-            if (connection != null) {
-                try {
-                    connection.rollback();
-                } catch (SQLException e1) {
-                    e1.printStackTrace();
-                }
+
+            try (PreparedStatement preparedStatement = this.preparedStatement(sql, connection, params)) {
+                updated = preparedStatement.executeUpdate();
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
             }
-            e.printStackTrace();
-        } finally {
-            this.closeConnection(connection);
+
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            log.error("", e);
         }
 
         return updated;
@@ -100,30 +88,23 @@ public class JdbcDao implements Dao {
     }
 
     public int[] updateBatch(String sql, Object[]... paramss) {
-        Connection connection = null;
-
         int[] updated = null;
-        try {
-            connection = this.getConnection();
+        try (Connection connection = this.dataSource.getConnection()){
             connection.setAutoCommit(false);
-            PreparedStatement preparedStatement = this.batchPreparedStatement(sql, connection, paramss);
-            updated = preparedStatement.executeBatch();
-            connection.commit();
-        } catch (SQLException e) {
-            if (connection != null) {
-                try {
-                    connection.rollback();
-                } catch (SQLException e1) {
-                    e1.printStackTrace();
-                }
+
+            try (PreparedStatement preparedStatement = this.batchPreparedStatement(sql, connection, paramss)) {
+                updated = preparedStatement.executeBatch();
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
             }
-            e.printStackTrace();
-        } finally {
-            this.closeConnection(connection);
+
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            log.error("", e);
         }
 
         return updated;
-
     }
 
     private Map<String, Object> toMap(ResultSet resultSet) throws SQLException {
@@ -159,23 +140,4 @@ public class JdbcDao implements Dao {
         return preparedStatement;
     }
 
-    private Connection getConnection() throws SQLException {
-
-        //    return DriverManager.getConnection(url, username, password);
-
-        return dataSource.getConnection();
-    }
-
-    private void closeConnection(Connection connection) {
-
-        if (connection != null) {
-            try {
-                connection.setAutoCommit(true);
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
 }
